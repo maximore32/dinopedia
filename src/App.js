a9591cdb48f0e4cde382061755b12be73a429ac6
maximore32 <78425799+maximore32@users.noreply.github.com>
2022-01-26 18:03:35 -0300

    xd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState,useEffect } from 'react';
 import { Switch,Route,Link,useParams } from 'react-router-dom'
 
+
+var titulin= "App informativa sobre dinosaurios"
+
 const Dino = () => {
   const [Dino, setUser] = useState([])
   const usuarios = []
   let params = useParams();
   const getUser = () => {
-    fetch('http://localhost:8080/api/'+params.ID)
+    fetch("https://diboos.herokuapp.com/api/id/"+params.ID)
       .then(response => response.json())
       .then(user => {usuarios.push(user);setUser(usuarios);})
       .catch(err => console.log(err.message))
@@ -22,9 +25,12 @@ const Dino = () => {
       <section>
           {Dino.map((u) =>
           <div key={u.ID}>
-          <h1>{ params.ID } Dinosaurio- {u.Dino}</h1>
-          <p>Altura: {u.Altitud}</p> 
-          <p>Longitud: {u.Longitud}</p> 
+          <h1 className='h1'>Dinosaurio- {u.Dino}</h1>
+          <img className='imagen' src={u.Imagen}></img>
+          <p className='parrafin'>Periodo: {u.Periodo}</p> 
+          <p className='parrafin'>Altura: {u.Altitud}</p> 
+          <p className='parrafin'>Longitud: {u.Longitud}</p>
+          <p className='x'>{u.Descripcion}</p>          
           <p><Link to="/">Regresar a la home</Link></p>
           </div>
           
@@ -40,10 +46,10 @@ const Dinosaurios = () => {
     const [users, setUsers] = useState( [])
   
     function getUsers() {
-      fetch('http://localhost:8080/api')
+      fetch('https://diboos.herokuapp.com/api')
         .then(response => response.json())
         .then(users => setUsers(users))
-        .catch(err => console.log("eror no indentificado"))
+        .catch(err => console.log("error no indentificado"))
     }
   
     useEffect(() => {
@@ -52,10 +58,15 @@ const Dinosaurios = () => {
   
     
 
-    return (  
-        <ul>
-          {users.map(user => <li key={user.ID}><Link to={"/Dinosaurio/"+user.ID}>{user.Dino}</Link><Link to={"/Dinosaurio/"+user.ID}><img src={user.Imagen} alt='Dinopedia'></img></Link></li>)}        
-        </ul>
+    return (
+        <main className='cuerpo'>
+          <div className='h2'><h2>{titulin}</h2></div>
+          {users.map(user => 
+          <div className='dinocajas' key={user.ID}>
+          <Link to={"/Dinosaurio/"+user.ID}><img  className="dinoimg" src={user.Imagen} alt='Dinopedia'></img></Link>
+          <h1 className='h1'>{user.Dino}</h1>          
+          </div>)}        
+        </main>
       
     )
   }
@@ -63,12 +74,13 @@ const Dinosaurios = () => {
   const App = () => (
         <div className="App">
           <header className="App-header">          
-            <h1 className="App-title">Dinos</h1>
+            <h1 className="App-title"><Link to="/">Dinopedia</Link></h1>
           </header>
           <Switch>
             <Route exact path="/"><Dinosaurios/></Route>
             <Route exact path="/Dinosaurio/:ID"><Dino/></Route>
-          </Switch>        
+          </Switch>
+          <footer><small>Dinopedia 2021</small></footer>        
         </div>
       );
 export default App;
